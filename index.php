d1a8f6203eaba2ca96af823c262281f9e2e39c92
uomma <marcelloleone975@gmail.com>
2023-04-17 09:44:42 +0200

    costruttore

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <?php
 
 class Movie{
+
+    //variabili
     public $titolo;
     public $genere;
     public $durata;
-    public $paese;
-}
+    public static $paese = 'usa';
 
+    //costruttore
+    function __construct(string $_titolo, string $_genere, int $_durata)
+    {
+        $this->titolo = $_titolo;
+        
+    }
+   /*  //metodo
+    public function  smt (){
+        return smt;
+    } */
+
+}   
+$Batman = new Movie('Batman', 'Azione', '120' );
+echo Movie ::$paese;
+
+ var_dump($Movie);    
+ 
